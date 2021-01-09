c1363f9a927f76ea49c611196f866bb5910a4f02
Philippe Ombredanne <pombredanne@nexb.com>
2021-01-09 15:24:14 +0100

    Use plain Apache license
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- tests/typecode/test_contenttype.py
+++ tests/typecode/test_contenttype.py
@@ -1,26 +1,24 @@
 #
-# Copyright (c) 2018 nexB Inc. and others. All rights reserved.
-# http://nexb.com and https://github.com/nexB/scancode-toolkit/
-# The ScanCode software is licensed under the Apache License version 2.0.
-# Data generated with ScanCode require an acknowledgment.
+# Copyright (c) nexB Inc. and others.
+# Visit https://github.com/nexB/typecode/
+# Visit https://nexb.com https://aboutcode.org and
+#  https://github.com/nexB/scancode-toolkit/ for support
 # ScanCode is a trademark of nexB Inc.
 #
-# You may not use this software except in compliance with the License.
-# You may obtain a copy of the License at: http://apache.org/licenses/LICENSE-2.0
-# Unless required by applicable law or agreed to in writing, software distributed
-# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-# CONDITIONS OF ANY KIND, either express or implied. See the License for the
-# specific language governing permissions and limitations under the License.
+# SPDX-License_identifier: Apache-2.0
 #
-# When you publish or redistribute any data created with ScanCode or any ScanCode
-# derivative work, you must accompany this data with the following acknowledgment:
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-#  Generated with ScanCode and provided on an "AS IS" BASIS, WITHOUT WARRANTIES
-#  OR CONDITIONS OF ANY KIND, either express or implied. No content created from
-#  ScanCode should be considered or used as legal advice. Consult an Attorney
-#  for any legal advice.
-#  ScanCode is a free software code scanning tool from nexB Inc. and others.
-#  Visit https://github.com/nexB/scancode-toolkit/ for support and download.
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 import os
 from unittest.case import expectedFailure
@@ -36,7 +34,6 @@ from commoncode.system import on_windows
 from typecode.contenttype import get_filetype
 from typecode.contenttype import get_pygments_lexer
 from typecode.contenttype import get_type
-
 
 # aliases for testing
 get_mimetype_python = lambda l: get_type(l).mimetype_python
@@ -58,7 +55,6 @@ is_link = lambda l: get_type(l).is_link
 is_broken_link = lambda l: get_type(l).is_broken_link
 size = lambda l: get_type(l).size
 contains_text = lambda l: get_type(l).contains_text
-
 
 
 class TestContentTypeComplex(FileBasedTesting):
